661ed39bd66bd5a210429faf20a7a3ac595a83c5
Hendrik Leppkes <h.leppkes@gmail.com>
2013-03-09 07:52:25 +0100

    Cleanup after a3cd5d9

--- modules/Player.lua
+++ modules/Player.lua
@@ -185,28 +185,26 @@ local function setBarTicks(ticknum)
 	end
 end
 
-local channelingTicks
-channelingTicks = {
-		-- warlock
-		[GetSpellInfo(1120)] = 6, -- drain soul
-		[GetSpellInfo(689)] = 6, -- drain life
-		[GetSpellInfo(103103)] = 4, -- malefic grasp
-		[GetSpellInfo(108371)] = 6, -- harvest life
-		-- druid
-		[GetSpellInfo(740)] = 4, -- Tranquility
-		[GetSpellInfo(16914)] = 10, -- Hurricane
-		[GetSpellInfo(106996)] = 10, -- Astral Storm
-		-- priest
-		[GetSpellInfo(15407)] = 3, -- mind flay
-		[GetSpellInfo(129197)] = 3, -- mind flay: insanity
-		[GetSpellInfo(48045)] = 5, -- mind sear
-		[GetSpellInfo(47540)] = 2, -- penance
-		-- mage
-		[GetSpellInfo(5143)] = 5, -- arcane missiles
-		[GetSpellInfo(10)] = 8, -- blizzard
-		[GetSpellInfo(12051)] = 3, -- evocation
+local channelingTicks = {
+	-- warlock
+	[GetSpellInfo(1120)] = 6, -- drain soul
+	[GetSpellInfo(689)] = 6, -- drain life
+	[GetSpellInfo(103103)] = 4, -- malefic grasp
+	[GetSpellInfo(108371)] = 6, -- harvest life
+	-- druid
+	[GetSpellInfo(740)] = 4, -- Tranquility
+	[GetSpellInfo(16914)] = 10, -- Hurricane
+	[GetSpellInfo(106996)] = 10, -- Astral Storm
+	-- priest
+	[GetSpellInfo(15407)] = 3, -- mind flay
+	[GetSpellInfo(129197)] = 3, -- mind flay: insanity
+	[GetSpellInfo(48045)] = 5, -- mind sear
+	[GetSpellInfo(47540)] = 2, -- penance
+	-- mage
+	[GetSpellInfo(5143)] = 5, -- arcane missiles
+	[GetSpellInfo(10)] = 8, -- blizzard
+	[GetSpellInfo(12051)] = 3, -- evocation
 }
-
 
 local function getChannelingTicks(spell)
 	if not db.showticks then
