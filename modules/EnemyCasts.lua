dd35f6dc5a348c7509380f858d3c4530edc48fb5
Hendrik Leppkes <h.leppkes@gmail.com>
2024-06-19 20:19:59 +0200

    Check if CR_HASTE_SPELL is available before using it

--- modules/EnemyCasts.lua
+++ modules/EnemyCasts.lua
@@ -178,7 +178,7 @@ function Enemy:CLEUHandler()
 		casts[sGUID].spellName = spellName
 		casts[sGUID].spellId = spellId
 		casts[sGUID].texture = texture
-		casts[sGUID].duration = castTime / 1000 * (1 + (GetCombatRatingBonus(CR_HASTE_SPELL) / 100))
+		casts[sGUID].duration = castTime / 1000 * (1 + (CR_HASTE_SPELL and (GetCombatRatingBonus(CR_HASTE_SPELL) / 100) or 0))
 		casts[sGUID].startTime = GetTime()
 		casts[sGUID].endTime = casts[sGUID].startTime + casts[sGUID].duration
 
